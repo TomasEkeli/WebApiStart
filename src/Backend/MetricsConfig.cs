54b15c92e5919b461453287fa17e1464039297ae
Tomas Ekeli <tomas@eke.li>
2023-11-04 12:28:19 +0000

    add otlp exporter to backend

--- src/Backend/MetricsConfig.cs
+++ src/Backend/MetricsConfig.cs
@@ -7,20 +7,20 @@ public static class MetricsConfig
     public static readonly Meter Meter = new(DiagnosticsConfig.Name);
 
     // example metrics we can add values to (change these to match your app's needs)
-    public static readonly Histogram<double> SalesValue = Meter
-        .CreateHistogram<double>(
+    public static readonly Histogram<double> SalesValue =
+        Meter.CreateHistogram<double>(
             name: "sales.value",
             unit: "currency",
             description: "the value of sales");
 
-    public static readonly Counter<int> Users = Meter
-        .CreateCounter<int>(
+    public static readonly Counter<int> Users =
+        Meter.CreateCounter<int>(
             name: "users.count",
             unit: "users",
             description: "the number of users registered");
 
-    public static readonly Counter<int> Orders = Meter
-        .CreateCounter<int>(
+    public static readonly Counter<int> Orders =
+        Meter.CreateCounter<int>(
             name: "sales.orders.count",
             unit: "orders",
             description: "the number of orders made");
