3ba853b51887cf8f9eea1fbc62d0ccd900f894e1
Tomas Ekeli <tomas@eke.li>
2023-11-04 11:18:14 +0000

    add some example metrics
    helps me remember how to set them up

---
+++ src/Backend/MetricsConfig.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics.Metrics;
+
+namespace Backend;
+
+public static class MetricsConfig
+{
+    public static readonly Meter Meter = new(DiagnosticsConfig.Name);
+
+    // example metrics we can add values to (change these to match your app's needs)
+    public static readonly Histogram<double> SalesValue = Meter
+        .CreateHistogram<double>(
+            name: "sales.value",
+            unit: "currency",
+            description: "the value of sales");
+
+    public static readonly Counter<int> Users = Meter
+        .CreateCounter<int>(
+            name: "users.count",
+            unit: "users",
+            description: "the number of users registered");
+}
