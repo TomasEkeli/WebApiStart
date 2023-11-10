4d69f59c71a5bff96b29ec16631e8dba50a21da3
Tomas Ekeli <te@davidhorn.com>
2023-11-10 15:14:40 +0000

    remove /version from open-api spec

--- src/Backend/MonitoringEndpoints.cs
+++ src/Backend/MonitoringEndpoints.cs
@@ -5,16 +5,19 @@ namespace Backend;
 
 public static class MonitoringEndpoints
 {
-    public static void MapMonitoringEndpoints(this WebApplication app)
-    {
-        app.MapGet("/version", () => Metadata.CurrentVersion());
-        app.MapHealthChecks(
-            "/healthz",
-            new()
-            {
-                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-            });
-        app.MapHealthChecks("/readyz");
+  public static void MapMonitoringEndpoints(this WebApplication app)
+  {
+    app
+      .MapGet("/version", () => Metadata.CurrentVersion())
+      // this endpoint is not part of the API, it is an infrastructure endpoint
+      .ExcludeFromDescription();
 
-    }
+    app.MapHealthChecks(
+      "/healthz",
+      new()
+      {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+      });
+    app.MapHealthChecks("/readyz");
+  }
 }
